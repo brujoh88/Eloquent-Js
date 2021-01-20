0db0996586f419fc50a63a36e76cebc78be85c73
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-01-20 02:00:00 -0300

    Ejercicio 1 resuelto Cap3

---
+++ cap3/1Minimum.js
@@ -0,0 +1,6 @@
+const min = (num1, num2) => (num1 > num2 ? num2 : num1)
+console.log(min(-1, 1))
+console.log(min(2, 1))
+console.log(min(1, 1))
+console.log(min(13, -10))
+console.log(min(-100, -5))
